d43c5e9f02d075b7ef831d4ad2324b28d0c5d961
Adrián Juárez <comecacahuates@yahoo.com>
2021-06-18 05:17:57 -0500

    Componente del formato de las páginas

---
+++ components/layout.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import Navbar from "../components/navbar";
+
+/**
+ * Propiedades del componente de formato de las páginas
+ */
+export interface LayoutProps {
+  /* Título de la página */
+  pageTitle: string;
+  /* Ruta de la página */
+  route: string;
+  /* Contenido */
+  children: React.ReactNode;
+}
+
+export default function Layout(props: LayoutProps) {
+  /* Propiedades */
+  const pageTitle: string = props.pageTitle;
+  const route: string = props.route;
+  const children: React.ReactNode = props.children;
+
+  /* Renderización */
+  return (
+    <div>
+      <Navbar activeItem={route} />
+      {children}
+    </div>
+  );
+}
