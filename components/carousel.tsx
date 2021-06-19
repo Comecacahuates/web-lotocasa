f2f568c8faccce43a9a37bbe58b99a838ac17dc1
Adrián Juárez <comecacahuates@yahoo.com>
2021-06-19 04:31:36 -0500

    Agregar contenedor al componente de carrusel

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -2,6 +2,7 @@
 import * as React from "react";
 import Image from "next/image";
 import { css } from "@emotion/react";
+import Container from "react-bootstrap/Container";
 import Carousel from "react-bootstrap/Carousel";
 import Button from "react-bootstrap/Button";
 import Icon from "../components/icon";
@@ -25,36 +26,38 @@ export default function (props: CarouselProps) {
 
   /* Renderización */
   return (
-    <Carousel>
-      {carousel.items.map((item: any, index: number) => (
-        <Carousel.Item
-          key={index}
-          css={css`
-            height: ${carousel.height};
-          `}
-        >
-          {/* Imagen */}
-          <Image
-            src={item.image.src}
-            className={`d-block w-100`}
-            layout="fill"
-            objectFit="cover"
-            objectPosition="50% 50%"
-          />
-          {/* Título y pie */}
-          {item.caption !== undefined ? (
-            <Carousel.Caption>
-              <h1 className="display-3">{item.caption.title}</h1>
-              <p className="h4">{item.caption.text}</p>
-              <Button href={item.caption.button.link} variant="outline-light">
-                {item.caption.button.label} <Icon icon="chevron-right" />
-              </Button>
-            </Carousel.Caption>
-          ) : (
-            ""
-          )}
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <Container className="p-0" fluid={carousel.wide || false}>
+      <Carousel>
+        {carousel.items.map((item: any, index: number) => (
+          <Carousel.Item
+            key={index}
+            css={css`
+              height: ${carousel.height};
+            `}
+          >
+            {/* Imagen */}
+            <Image
+              src={item.image.src}
+              className={`d-block w-100`}
+              layout="fill"
+              objectFit="cover"
+              objectPosition="50% 50%"
+            />
+            {/* Título y pie */}
+            {item.caption !== undefined ? (
+              <Carousel.Caption>
+                <h1 className="display-3">{item.caption.title}</h1>
+                <p className="h4">{item.caption.text}</p>
+                <Button href={item.caption.button.link} variant="outline-light">
+                  {item.caption.button.label} <Icon icon="chevron-right" />
+                </Button>
+              </Carousel.Caption>
+            ) : (
+              ""
+            )}
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </Container>
   );
 }
