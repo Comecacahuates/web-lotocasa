f4bb7af63025853b072a781cf982997e0f21e93f
Adrián Juárez <comecacahuates@yahoo.com>
2021-06-19 03:33:09 -0500

    Corrección de error en la barra de navegación
    Se eliminó importar un componente que ya no existe, lo que provocaba un
    error en el despliegue del proyecto

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,6 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Container from "react-bootstrap/Container";
-import NavbarDropdown from "../components/navbardropdown";
 
 import navbarItems from "../content/navbar.json";
 
