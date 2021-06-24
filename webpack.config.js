4bb5682f5f0d6618974ce3d8225341f31239eb8e
Adrián Juárez <comecacahuates@yahoo.com>
2021-06-24 09:40:56 -0500

    Archivo webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  entry: ["./app/main.js"],
+  output: {
+    path: __dirname + "/dist",
+    filename: "app.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  // plugins: []
+};
