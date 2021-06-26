5bb5eff7b7f0b293b60594cd19e1a75f8b8e7f83
Adrián Juárez <comecacahuates@yahoo.com>
2021-06-26 06:18:26 -0500

    Validaciones en el formulario de contacto

--- components/ui/contactform.tsx
+++ components/ui/contactform.tsx
@@ -5,11 +5,30 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Icon from "./icon";
 import { ContactForm } from "../../@types/pagestructure";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 /** Propiedades del componente de formulario de contacto */
 export interface ContactFormProps {
   contactForm: ContactForm;
 }
+
+/** Esquema de validación */
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .max(50, "El nombre no puede ser mayor a 50 carácteres")
+    .required("El nombre es requerido"),
+  email: Yup.string()
+    .email("La dirección de correo no es válida")
+    .required("El correo electrónico es requerido"),
+  subject: Yup.string()
+    .max(100, "El asunto no puede ser mayor a 100 carácteres")
+    .required("El asunto es requerido"),
+  message: Yup.string()
+    .min(30, "El mensaje es muy corto")
+    .max(1000, "El mensaje debe ser mayor a 1000 carácteres")
+    .required("El mensaje no puede estar vacío"),
+});
 
 /** Componente de formulario de contacto */
 export default function ContactFormUI(props: ContactFormProps) {
@@ -28,35 +47,73 @@ export default function ContactFormUI(props: ContactFormProps) {
 
   /* Renderización */
   return (
-    <Form className="mb-3" action={action} method="POST">
-      <input type="hidden" name="_subject" value="Test" />
-      <input type="hidden" name="_captcha" value={captcha.toString()} />
-      <input type="hidden" name="_next" value={next} />
-      <Row>
-        {/* Nombre */}
-        <Form.Group className="mb-3" as={Col} xs={12} sm={6}>
-          <Form.Label>{nameLabel}</Form.Label>
-          <Form.Control name="name" />
-        </Form.Group>
-        {/* Correo */}
-        <Form.Group className="mb-3" as={Col} xs={12} sm={6}>
-          <Form.Label>{emailLabel}</Form.Label>
-          <Form.Control type="email" name="email" />
-        </Form.Group>
-        {/* Asunto */}
-        <Form.Group className="mb-3" as={Col} xs={12}>
-          <Form.Label>{subjectLabel}</Form.Label>
-          <Form.Control name="subject" />
-        </Form.Group>
-      </Row>
-      {/* Mensaje */}
-      <Form.Group className="mb-3">
-        <Form.Label>{messageLabel}</Form.Label>
-        <Form.Control as="textarea" name="message" rows={5} />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        {submitLabel} <Icon icon="arrow-right" />
-      </Button>
-    </Form>
+    <Formik
+      validationSchema={validationSchema}
+      initialValues={{ name: "", email: "", subject: "", message: "", next }}
+      /* TODO: Enviar el formulario */
+      onSubmit={(values) => alert(JSON.stringify(values, null, 2))}
+    >
+      {({ handleSubmit, values, touched, errors, getFieldProps }) => (
+        <Form className="mb-3" noValidate onSubmit={handleSubmit}>
+          <Row>
+            {/* Nombre */}
+            <Form.Group className="mb-3" as={Col} xs={12} sm={6}>
+              <Form.Label>{nameLabel}</Form.Label>
+              <Form.Control
+                value={values.name}
+                {...getFieldProps("name")}
+                isInvalid={touched.name && !!errors.name}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.name}
+              </Form.Control.Feedback>
+            </Form.Group>
+            {/* Correo */}
+            <Form.Group className="mb-3" as={Col} xs={12} sm={6}>
+              <Form.Label>{emailLabel}</Form.Label>
+              <Form.Control
+                type="email"
+                value={values.email}
+                {...getFieldProps("email")}
+                isInvalid={touched.email && !!errors.email}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          {/* Asunto */}
+          <Form.Group className="mb-3" as={Col} xs={12}>
+            <Form.Label>{subjectLabel}</Form.Label>
+            <Form.Control
+              type="text"
+              value={values.subject}
+              {...getFieldProps("subject")}
+              isInvalid={touched.subject && !!errors.subject}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.subject}
+            </Form.Control.Feedback>
+          </Form.Group>
+          {/* Mensaje */}
+          <Form.Group className="mb-3">
+            <Form.Label>{messageLabel}</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              value={values.message}
+              {...getFieldProps("message")}
+              isInvalid={touched.message && !!errors.message}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            {submitLabel} <Icon icon="chevron-right" />
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 }
