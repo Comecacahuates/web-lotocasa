3f59dcbe866f05a264dad58dcc184e51beb64c50
Adrián Juárez <comecacahuates@yahoo.com>
2021-06-30 01:30:52 -0500

    Depuración de CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
+const glob = require("glob");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+
 module.exports = {
   entry: ["./app/main.js"],
   output: {
     path: __dirname + "/dist",
     filename: "app.bundle.js",
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true,
+        },
+      },
+    },
   },
   module: {
     rules: [
@@ -16,6 +32,7 @@ module.exports = {
       {
         test: /\.((c|sa|sc)ss)$/i,
         use: [
+          MiniCssExtractPlugin.loader,
           "style-loader",
           {
             loader: "css-loader",
@@ -33,5 +50,12 @@ module.exports = {
       },
     ],
   },
-  // plugins: []
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    }),
+  ],
 };
